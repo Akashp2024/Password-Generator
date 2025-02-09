8699f679c5b5f358c08c2e7d9ace7dd2f8515acc
Akashp <2024akashp@gmail.com>
2025-02-09 19:22:07 +0530

    UI part completed

--- Password-Generator/src/App.jsx
+++ Password-Generator/src/App.jsx
@@ -1,35 +1,80 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useCallback } from "react";
+
+// import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [length, setLength] = useState(8);
+  const [numallowed, setnumallowed] = useState(false);
+  const [spcharallowed, setspcharallowed] = useState(false);
+  const [password, setPassword] = useState("");
+
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (numallowed) {
+      str += "0123456789";
+    }
+    if (spcharallowed) {
+      str += "!@#$%^&*()_+";
+    }
+
+    for (let i = 1; i <= length; i++) {
+      pass = str.charAt(Math.floor(Math.random() * str.length + 1));
+    }
+    setPassword(pass);
+  }, [length, numallowed, spcharallowed, setPassword]);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="w-screen h-screen bg-blue-500 flex flex-col justify-center items-center">
+        <h1 className="text-4xl text-center font-sans p-5 w-1/2 bg-white rounded-lg">
+          Password Generator
+        </h1>
+        <div className="main_container bg-white w-1/2 h-1/3 mt-4 rounded-lg p-6">
+          <div className="sub_container1 flex flex-row space-x-4 p-2 w-full h-20">
+            <input
+              type="text"
+              className="p-2 border rounded flex-grow"
+              style={{ flexGrow: 3 }}
+            />
+            <button
+              className="bg-blue-300 p-2 rounded flex-grow hover:bg-blue-500 text-2xl"
+              style={{ flexGrow: 1 }}
+            >
+              Copy
+            </button>
+          </div>
+          <div className="sub_container2 flex flex-row space-x-4 mt-4 w-full text-2xl">
+            <div className="flex items-center w-full">
+              <input
+                type="range"
+                id="lengthrange"
+                min={5}
+                max={15}
+                value={length}
+                onChange={(e) => setLength(e.target.value)}
+              />
+              <label htmlFor="lengthrange" className="ml-2">
+                Length: {length}
+              </label>
+            </div>
+            <div className="flex items-center w-full">
+              <input type="checkbox" id="Numallowed" />
+              <label htmlFor="Numallowed" className="ml-2">
+                Numbers
+              </label>
+            </div>
+            <div className="flex items-center w-full">
+              <input type="checkbox" id="Spcharallowed" />
+              <label htmlFor="Spcharallowed" className="ml-2">
+                Special Characters
+              </label>
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
